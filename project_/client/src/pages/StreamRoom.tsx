7ce8308cd9159b40d66cc16e724cac3884266dc9
Manthan Sharma <manthan2306s@gmail.com>
2024-06-23 15:30:52 +0530

    re

--- project_/client/src/pages/StreamRoom.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { useParams } from "react-router-dom";
-
-const StreamRoom = () => {
-  const { roomId } = useParams();
-  return <div>{roomId}</div>;
-};
-
-export default StreamRoom;
--- project_/server/src/config/env.var.ts
+++
@@ -1,1 +0,0 @@
-export const REDIS_PORT = 8200;
